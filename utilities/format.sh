b48e4ac2c3567073d426ae687de38caab01dc98e
Luke <schuurml@mcmaster.ca>
2026-04-13 08:56:16 -0400

    Add heading and label formatter

--- utilities/format.sh
+++ utilities/format.sh
@@ -4,4 +4,7 @@
 # Find all .tex files in documents/ directory
 find documents -name "*.tex" -type f -exec latexindent -s -w -l utilities/format.yaml -c=build/ -m {} \;
 
+# Normalize headings: join multi-line titles, split \label onto its own line
+find documents -name "*.tex" -type f -print0 | xargs -0 python3 utilities/normalize-headings.py
+
 echo "Formatting complete"
